90f086ed8a0fbd8243f08106c3d54f8b302f6703
unknown <villejobsv10@gmail.com>
2019-09-26 17:41:37 +0200

    .php

---
+++ smr2b.php
@@ -0,0 +1,12 @@
+<a href="http://192.168.0.105/smr2b_cesar/smr2b.php">cesar</a>
+VILLEHASH
+<a href="http://192.168.0.108/smr2b_jesuscrespo/smr2b.php">Jesus</a>
+
+<?php
+
+$f=fopen('visitas.txt' , 'a');
+fwrite ($f,$_SERVER['REMOTE_ADDR']);
+fwrite ($f, "\r\n");
+fclose($f);
+
+?>
