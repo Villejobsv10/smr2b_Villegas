a14c1c5eab318ca1b45f5362b5bd7df5cafcc03e
unknown <villejobsv10@gmail.com>
2019-10-01 17:43:52 +0200

    Formularios

--- smr2b.php
+++ smr2b.php
@@ -2,12 +2,24 @@
 VILLEHASH
 <a href="http://192.168.0.108/smr2b_jesuscrespo/smr2b.php">Jesus</a>
 
-<?php
+<form>
+	<input name="email" type="email">
+	<button>Enviar</button>
+</form>
 
+<?php
 $f=fopen('visitas.txt' , 'a');
 fwrite($f,date('Y-m-d H:i:s '));
 fwrite ($f,$_SERVER['REMOTE_ADDR']);
 fwrite ($f, "\r\n");
 fclose($f);
+?>
 
+<?php
+if(isset($_GET['email'])){
+	$f=fopen('suscriptores.txt','a');
+	fwrite($f,$_GET['email']);
+	fwrite($f,"\r\n");
+	fclose($f);
+}
 ?>
